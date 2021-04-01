7b464e6306ef3c3d7ecaf52c8b615d98c650b2a8
hunterkeating <76222438+hunterkeating@users.noreply.github.com>
2021-04-01 08:08:50 -0400

    Update OSC_server.py

--- OSC_server.py
+++ OSC_server.py
@@ -2,12 +2,10 @@ from pythonosc.udp_client import SimpleUDPClient
 
 def send_angle(message):
     ip = "127.0.0.1" # 127.0.0.1 is localhost
-    port = 6789
+    port = 6789 # This  port must math the port specified in plugin
 
     client = SimpleUDPClient(ip, port) # Creating the OSC UDP Client
 
     #print("Sending to Max")
     client.send_message(":", message)
     #print("Angle Sent")
-
-# send_angle('157') # For debugging
